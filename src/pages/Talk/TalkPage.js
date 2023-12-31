f1c9670491c2c4f69d33dab92da4e5ed35e3e8b4
mynuni <elelaka0000@gmail.com>
2023-12-31 21:10:26 +0900

    Fix: 연결 중 언마운트 시 connection established yet 오류 방지

--- src/pages/Talk/TalkPage.js
+++ src/pages/Talk/TalkPage.js
@@ -7,14 +7,17 @@ import {API_PATHS} from "../../constants/path";
 import Stomp from "stompjs";
 import {useRecoilValue} from "recoil";
 import {userSelector} from "../../recoil/user";
+import {useQueryClient} from "react-query";
 
 const TalkPage = () => {
 
         const [selectedChatRoom, setSelectedChatRoom] = useState();
+        const [selectedChatRoomData, setSelectedChatRoomData] = useState({});
         const stompClientRef = useRef(null);
         const [currentSubscribe, setCurrentSubscribe] = useState(null);
         const [messages, setMessages] = useState([]);
         const user = useRecoilValue(userSelector);
+        const queryClient = useQueryClient();
 
         const handleChangeChatRoom = (chatRoomId) => {
             setSelectedChatRoom(chatRoomId);
@@ -23,14 +26,15 @@ const TalkPage = () => {
         useEffect(() => {
             const socket = new SockJS(process.env.REACT_APP_SERVER_BASE_URL + API_PATHS.WEB_SOCKET_CHAT);
             stompClientRef.current = Stomp.over(socket);
-
+            stompClientRef.current.debug = null; // 콘솔에 디버그 메세지 출력 제거
             stompClientRef.current.connect({
                 Authorization: "Bearer " + localStorage.getItem(process.env.REACT_APP_ACCESS_TOKEN_STORAGE_KEY)
             }, null);
 
             return () => {
-                // 언마운트 시 자원 반환
-                stompClientRef.current.disconnect();
+                if (stompClientRef.current.connected) {
+                    stompClientRef.current.disconnect();
+                }
             };
 
         }, []);
@@ -39,9 +43,15 @@ const TalkPage = () => {
             if (currentSubscribe) currentSubscribe.unsubscribe();
             const headers = {participant_id: user.id};
 
-            const subscribe = stompClientRef.current.subscribe(`/topic/chat/rooms/${chatRoomId}`, (data) => {
+            const subscribe = stompClientRef.current.subscribe(`/topic/chat/rooms/${chatRoomId}`, (message) => {
                 // 받은 메세지를 배열의 맨 앞에 붙인다.
-                setMessages(prevState => [...prevState, JSON.parse(data.body)]);
+                const messageBody = JSON.parse(message.body);
+                setMessages(prevState => [...prevState, messageBody]);
+
+                if (["ENTER", "LEAVE"].includes(messageBody.chatMessageType)) {
+                    queryClient.invalidateQueries("getChatRooms");
+                }
+
             }, headers);
 
             setCurrentSubscribe(subscribe);
@@ -54,18 +64,32 @@ const TalkPage = () => {
             }
         }, [selectedChatRoom]);
 
-        const handleSendMessage = (message) => {
+        const handleSendMessage = (content, type) => {
+
+            const message = {
+                chatRoomId: selectedChatRoom,
+                content: content,
+                senderId: user.id,
+                senderNickname: user.nickname,
+                senderProfileImage: user.profileImgPath,
+                chatMessageType: type
+            };
+
             stompClientRef.current.send(`/pub/chat/rooms/${selectedChatRoom}`, {}, JSON.stringify(message));
         };
 
         return (
             <ChatContainer>
                 <ChatRoomContainer selectedChatRoom={selectedChatRoom}
+                                   setSelectedChatRoomData={setSelectedChatRoomData}
                                    handleChangeChatRoom={handleChangeChatRoom}
-                                   subscribeChatRoom={subscribeChatRoom}/>
+                                   subscribeChatRoom={subscribeChatRoom}
+                                   handleSendMessage={handleSendMessage}
+                                   stompClientRef={stompClientRef}/>
                 <ChatMessageContainer messages={messages}
                                       setMessages={setMessages}
                                       selectedChatRoom={selectedChatRoom}
+                                      selectedChatRoomData={selectedChatRoomData}
                                       handleSendMessage={handleSendMessage}
                                       handleChangeChatRoom={handleChangeChatRoom}/>
             </ChatContainer>
@@ -79,5 +103,5 @@ const ChatContainer = styled.div`
     display: flex;
     justify-content: center;
     width: 100%;
-    height: 100%;
+    height: calc(100vh - 60px);
 `;
