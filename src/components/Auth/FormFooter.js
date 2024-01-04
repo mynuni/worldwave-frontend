848470deb8f5e8668c639c0db5e76b5a2fabc0e8
mynuni <elelaka0000@gmail.com>
2024-01-04 21:39:44 +0900

    Design: Footer에 게스트 로그인 추가

--- src/components/Auth/FormFooter.js
+++ src/components/Auth/FormFooter.js
@@ -3,13 +3,26 @@ import {NavLink} from "react-router-dom";
 import styled from "styled-components";
 import COLOR from "../../constants/color";
 import {CLIENT_PATHS} from "../../constants/path";
+import {loginGuest} from "../../apis/service/auth";
 
 const FormFooter = () => {
+
+    const handleLoginGuest = async () => {
+        try {
+            const response = await loginGuest();
+            const {accessToken} = response;
+            localStorage.setItem(process.env.REACT_APP_ACCESS_TOKEN_STORAGE_KEY, accessToken);
+            window.location.href = CLIENT_PATHS.EXPLORE;
+        } catch (error) {
+            alert(error.response?.data?.message || "게스트 로그인 실패");
+        }
+    }
+
     return (
         <StyledList>
             <StyledNavLink to={CLIENT_PATHS.SIGN_UP}>회원가입</StyledNavLink>
             <StyledNavLink to={CLIENT_PATHS.RESET_PASSWORD}>비밀번호 찾기</StyledNavLink>
-            <StyledNavLink to={process.env.REACT_APP_QNA_EMAIL}>문의하기</StyledNavLink>
+            <div onClick={handleLoginGuest} style={{cursor: "pointer"}}>GUEST</div>
         </StyledList>
     );
 };
